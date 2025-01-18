b96d376e842cc0e9a2ee2c4f34243c91f6332743
Jasim Uddin <jasimbdpro@gmail.com>
2025-01-18 08:54:08 +0600

    .

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'offline-cache-v1';
+const URLS_TO_CACHE = ['index.html'];
+
+// Install the service worker and cache the assets
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Opened cache');
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+});
+
+// Fetch the resources from the cache or fallback to the network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Update the service worker
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
